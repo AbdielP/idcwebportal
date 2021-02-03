b2fc3b6ea0284acedfdf93d56292076d8ec64873
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-03 13:45:04 -0500

    Se agrega guard login en public.routing.module y private.routing.module

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from 'src/app/services/guards/login.guard';
 import { PublicComponent } from './public.component';
 
 const routes: Routes = [
-  { path: '', component: PublicComponent, children: [
+  { path: '', canActivate: [LoginGuard], component: PublicComponent, children: [
     {
       path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
     }
