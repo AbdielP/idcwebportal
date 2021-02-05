41abe1315fce2ab7769578cfb4d520f489bd501c
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-05 14:04:42 -0500

    Se crea componente 'info' para dashboard. se agregan estilos básicos

--- src/app/admin/dashboard/dashboard.module.ts
+++ src/app/admin/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, InfoComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
