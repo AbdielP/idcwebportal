2dda4ff7ee91615de2a06bc1412c80ab4d7648d5
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-04 21:12:29 -0500

    Iniciando estilos html para dashboard admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AdminComponent, NavbarComponent],
+  declarations: [AdminComponent, NavbarComponent, SidebarComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
