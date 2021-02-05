df5370ca233a71e696c7731fe966f7cd1e0cfd99
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-04 20:45:46 -0500

    Navbar basico colocado

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,8 +4,7 @@ import { LoginGuard } from 'src/app/services/guards/login.guard';
 import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
-  // { path: 'admin', canActivate: [LoginGuard], component: AdminComponent, children: [
-  { path: 'admin', component: AdminComponent, children: [
+  { path: 'admin', canActivate: [LoginGuard], component: AdminComponent, children: [
     {
       path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
     }
