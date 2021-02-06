3e4f7ea7664b6084f037c90b095f518f5b1dba44
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-06 12:59:36 -0500

    Se inicia estilos para componente accesos

--- src/app/admin/accesos/accesos.module.ts
+++ src/app/admin/accesos/accesos.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AccesosRoutingModule } from './accesos-routing.module';
 import { AccesosComponent } from './accesos.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 @NgModule({
-  declarations: [AccesosComponent],
+  declarations: [AccesosComponent, BusquedaComponent],
   imports: [
     CommonModule,
     AccesosRoutingModule
