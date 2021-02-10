ff15767efe3f9353fb68466154ec143241dbd0c0
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-09 21:15:26 -0500

    Se integra codigo QR en detalles de accesos con el modulo npm qrcode.

--- src/app/admin/accesos/detalle/detalle.component.ts
+++ src/app/admin/accesos/detalle/detalle.component.ts
@@ -1,6 +1,9 @@
 import { Observable, Subscription } from 'rxjs';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SeguridadService } from 'src/app/services/seguridad.service';
+import html2canvas from 'html2canvas';
+import QRCode from 'qrcode';
+
 
 @Component({
   selector: 'app-detalle',
@@ -9,6 +12,11 @@ import { SeguridadService } from 'src/app/services/seguridad.service';
 })
 export class DetalleComponent implements OnInit {
 
+  @ViewChild('screen', { static: false}) screenx: ElementRef;
+  @ViewChild('canvas', { static: false}) canvasx: ElementRef;
+  @ViewChild('downloadLink', { static: false}) downloadLinkx: ElementRef;
+
+  qrcode: any = '';
   acceso: any = [];
   eventSubscription: Subscription;
   @Input() events: Observable<any>;
@@ -29,8 +37,31 @@ export class DetalleComponent implements OnInit {
       this.seguridadService.select(`sp_select_accesos_detalle('${detalle}')`).subscribe((resp: any) => {
         console.log(resp);
         this.acceso = resp.select[0];
+        this.generarQR(this.acceso);
       });
     });
+  }
+
+  // Genera el código QR
+  generarQR(info: any): void {
+    QRCode.toDataURL(`http://cwp-idc-sca/seguridad/agentsec/reg_visita_in.php?cedula_visitante=${info.cedula_visitante}&button=Buscar`)
+    .then(url => {
+      this.qrcode = url;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  // Genera un CANVAS a partir del HTML con el formato de QR IDC
+  downloadImage(name: string, id: string) {
+    html2canvas(this.screenx.nativeElement).then(canvas => {
+      this.canvasx.nativeElement.src = canvas.toDataURL();
+      this.downloadLinkx.nativeElement.href = canvas.toDataURL('image/png');
+      this.downloadLinkx.nativeElement.download = `QR_Code_${name}_${id}.png`;
+      this.downloadLinkx.nativeElement.click();
+    });
+    return;
   }
 
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,6 +56,8 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+// tslint:disable-next-line:no-string-literal
+(window as any)['global'] = window;
 
 
 /***************************************************************************************************
