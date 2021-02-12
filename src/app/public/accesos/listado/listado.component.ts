7080665d14538c33180be49c2e2ffa3f1cf41255
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-12 00:49:04 -0500

    Se crea componente listado en acceso de clientes. pendiente crear modulo para componentes compartidos en toda la apliacacion

--- src/app/public/accesos/listado/listado.component.ts
+++ src/app/public/accesos/listado/listado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GeneralService } from 'src/app/services/general.service';
 import { LocalstorageService } from 'src/app/services/localstorage/localstorage.service';
+import { SeguridadService } from 'src/app/services/seguridad.service';
 
 @Component({
   selector: 'app-listado',
@@ -11,35 +12,45 @@ export class ListadoComponent implements OnInit {
 
   accesos = [];
   userInfo: any;
-  userProyects: any;
+  userProyects: any = '';
 
-  constructor(private localStorageService: LocalstorageService, private generalService: GeneralService) { }
+  constructor(private localStorageService: LocalstorageService, private generalService: GeneralService,
+              private seguridadService: SeguridadService) { }
 
   ngOnInit() {
     this.getTokenInfo(this.localStorageService.getToken());
   }
 
-  onClickDetalleAcceso(idseguridad): void {
+  onClickDetalleAcceso(idseguridad: number): void {
+    console.log(idseguridad);
   }
 
+  // Obtiene la información del token del usuario logeado
   getTokenInfo(token: string) {
     this.localStorageService.getTokenInfo(token).subscribe((resp: any) => {
-      console.log(resp.tokeninfo);
       this.userInfo = resp.tokeninfo;
       this.getUsuarioProyectos(this.userInfo.idusuario);
     });
   }
 
+  // Lista los proyectos relacionados con el usuario logeado
   getUsuarioProyectos(idusuario: number): void {
     this.generalService.select(`sp_clientes_select_proyectos('${idusuario}')`).subscribe((resp: any) => {
       // console.log(resp);
       this.userProyects = resp.select;
       console.log(this.userProyects);
-      /*
-         - Hacer un menú de algún tipo para seleccionar que proyecto listar sus accesos.
-          - Al hacer click al proyecto, listar.
-          - Si tiene 1 solo proyecto, listar directamente.
-      */
+      if (this.userProyects.lengt === 1) {
+        this.selectAccesosProyecto(this.userProyects[0].compania_visitante, this.userProyects[0].datacenter);
+      }
+    });
+  }
+
+  // LLama los accesos del proyecto indicado
+  selectAccesosProyecto(companiavisitante: string, datacenter: string): void {
+    this.seguridadService.select(`sp_select_accesos_compania('${companiavisitante}', '${datacenter}')`)
+    .subscribe((resp: any) => {
+      console.log(resp);
+      this.accesos = resp.select;
     });
   }
 
