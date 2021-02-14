5a42999681ce0221796257e353e70995ff3d0ae7
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-14 02:43:03 -0500

    Se modifica aspecto de componente 'detalles' de acceso

--- src/app/public/accesos/accesos.module.ts
+++ src/app/public/accesos/accesos.module.ts
@@ -7,6 +7,7 @@ import { AccesosComponent } from './accesos.component';
 import { SharedModule } from '../shared/shared.module';
 import { ListadoComponent } from './listado/listado.component';
 import { CompartidosModule } from 'src/app/compartidos/compartidos.module';
+import { QrcodeComponent } from 'src/app/compartidos/accesos/qrcode/qrcode.component';
 
 @NgModule({
   declarations: [AccesosComponent, ListadoComponent],
@@ -16,6 +17,6 @@ import { CompartidosModule } from 'src/app/compartidos/compartidos.module';
     CommonModule,
     AccesosRoutingModule,
     MatDialogModule
-  ]
+  ], entryComponents: [QrcodeComponent]
 })
 export class AccesosModule { }
