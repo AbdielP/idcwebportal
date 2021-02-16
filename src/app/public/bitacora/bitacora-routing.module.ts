355d29d1892175f3c934b2978c6512a4a4e61f52
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-15 23:40:19 -0500

    Se inicializa componente 'bitacora' para listar historial de visitas de los clientes al IDC'

---
+++ src/app/public/bitacora/bitacora-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BitacoraComponent } from './bitacora.component';
+
+const routes: Routes = [
+  { path: '', component: BitacoraComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BitacoraRoutingModule { }
--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -6,12 +6,9 @@ import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   { path: '', canActivate: [LoginGuard, ClienteGuard], component: PublicComponent, children: [
-    {
-      path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-    },
-    {
-      path: 'access', loadChildren: () => import('./accesos/accesos.module').then(m => m.AccesosModule)
-    }
+    { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+    { path: 'access', loadChildren: () => import('./accesos/accesos.module').then(m => m.AccesosModule) },
+    { path: 'bitacora', loadChildren: () => import('./bitacora/bitacora.module').then(m => m.BitacoraModule) }
   ]}
 ];
 
