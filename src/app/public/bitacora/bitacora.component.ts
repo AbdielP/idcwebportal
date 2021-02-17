522570b05330353ff33a16a21e63b37ce6363280
AbdielP <abdiel.pinzonc@gmail.com>
2021-02-16 21:06:32 -0500

    Tabla de materials funciona junto con el filtrado. fatta adaptar la paginacion...

--- src/app/public/bitacora/bitacora.component.ts
+++ src/app/public/bitacora/bitacora.component.ts
@@ -6,14 +6,14 @@ import { GeneralService } from 'src/app/services/general.service';
 import { LocalstorageService } from 'src/app/services/localstorage/localstorage.service';
 
 /** Constants used to fill up our data base. */
-const COLORS: string[] = [
-  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-  'aqua', 'blue', 'navy', 'black', 'gray'
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
+// const COLORS: string[] = [
+//   'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+//   'aqua', 'blue', 'navy', 'black', 'gray'
+// ];
+// const NAMES: string[] = [
+//   'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+//   'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+// ];
 
 @Component({
   selector: 'app-bitacora',
@@ -21,7 +21,7 @@ const NAMES: string[] = [
   styleUrls: ['./bitacora.component.css']
 })
 export class BitacoraComponent implements OnInit, AfterViewInit  {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
+  displayedColumns: string[] = ['nombre_visitante', 'check_in_out', 'empleado_visitante'];
   dataSource: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
@@ -29,13 +29,15 @@ export class BitacoraComponent implements OnInit, AfterViewInit  {
 
   userInfo: any;
   userProyects: any = '';
+  proyecto: any = '';
+  bitacora: any = '';
 
   constructor(private generalService: GeneralService, private localstorageService: LocalstorageService) {
      // Create 100 users
-     const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    //  const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
      // Assign the data to the data source for the table to render
-     this.dataSource = new MatTableDataSource(users);
+     this.dataSource = new MatTableDataSource(this.bitacora);
   }
 
   ngOnInit() {
@@ -55,16 +57,23 @@ export class BitacoraComponent implements OnInit, AfterViewInit  {
   // Lista los proyectos relacionados con el usuario logeado
   getUsuarioProyectos(idusuario: number): void {
     this.generalService.select('ggggwwwwpppp', `sp_clientes_select_proyectos('${idusuario}')`).subscribe((resp: any) => {
-      console.log(resp);
       this.userProyects = resp.select;
       if (this.userProyects.lengt === 1) {
-        // Llamar aquí
+        this.selectBitacoraProyecto(this.userProyects[0]);
       }
     });
   }
 
-  getBitacoraCliente() {
-  // CONSEGUIR los proyectos donde el usuario esté relacionado
+  selectBitacoraProyecto(proyecto: any): void {
+    this.proyecto = proyecto;
+    this.generalService.select('ggggwwwwpppp', `check_in_out.sp_select_bitacora_compania('${proyecto.nombre_empresa}',
+    '${proyecto.datacenter}','2021')`).subscribe((resp: any) => { // LLAMAR FECHA SERVICE PARA EL AÑO ACTUAL
+      console.log(resp);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.bitacora = resp.select;
+      this.dataSource = new MatTableDataSource(this.bitacora);
+    });
   }
 
   // HASTA AQUÍ NO BORRAR ----------------------->
@@ -86,14 +95,14 @@ export class BitacoraComponent implements OnInit, AfterViewInit  {
 }
 
 /** Builds and returns a new User. */
-function createNewUser(id: number): any {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+// function createNewUser(id: number): any {
+//   const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+//       NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
+//   return {
+//     id: id.toString(),
+//     name: name,
+//     progress: Math.round(Math.random() * 100).toString(),
+//     color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+//   };
+// }
