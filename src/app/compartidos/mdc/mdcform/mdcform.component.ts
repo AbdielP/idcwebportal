42831ff5147e75ee65693eadad2aaad67de236ce
AbdielP <abdiel.pinzonc@gmail.com>
2021-03-03 22:52:26 -0500

    Se filtra tipo de cuenta según el roll en formulario MDC. se coloca limite de fecha para versión de formulario MDC

--- src/app/compartidos/mdc/mdcform/mdcform.component.ts
+++ src/app/compartidos/mdc/mdcform/mdcform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { DateService } from 'src/app/services/date.service';
+import { GeneralService } from 'src/app/services/general.service';
 import { LocalstorageService } from 'src/app/services/localstorage/localstorage.service';
 
 @Component({
@@ -10,53 +11,82 @@ import { LocalstorageService } from 'src/app/services/localstorage/localstorage.
 })
 export class MdcformComponent implements OnInit {
 
+  hoy: any;
+  mdcForm: any;
   userroll: number;
+  roles = [];
 
-  constructor(private formBuilder: FormBuilder, private dateService: DateService, private localstorageService: LocalstorageService) { }
-
-  mdcForm = this.formBuilder.group({
-    username: ['', {validators: [Validators.required]}],
-    password: [{value: 'welcome', disabled: true}],
-    nombre: ['', {validators: [Validators.required]}],
-    apellido: ['', {validators: [Validators.required]}],
-    cedula: [null],
-    correo: ['', {validators: [Validators.required]}],
-    telefono: [''],
-    celular: [''],
-    rollId: ['', {validators: [Validators.required]}],
-    direccion: [''],
-    cargo: [''],
-    departamento: [''],
-    createdAt: this.dateService.getDate(),
-    check_activo: [false],
-
-    proyecto: ['', {validators: [Validators.required]}],
-    version: [this.dateService.actualYear(), {validators:
-      [Validators.required, Validators.min(2000), Validators.max(this.dateService.actualYear() + 1)]}],
-    solicitar_acceso: [false],
-    actualizar_acceso: [false],
-    eliminar_acceso: [false],
-    manos_remotas: [false],
-    alertas_baja: [false],
-    alertas_media: [false],
-    alertas_alta: [false],
-    editar_matriz: [false],
-    proyectos: this.formBuilder.array([]), // FormArray
-    link: ['', {validators: [Validators.required]}]
-  });
+  constructor(private formBuilder: FormBuilder, public dateService: DateService, private localstorageService: LocalstorageService,
+              private generalService: GeneralService) {
+    this.hoy = this.dateService.getDate();
+  }
 
   ngOnInit(): void {
+    this.mdcForm = this.formBuilder.group({
+      username: ['', {validators: [Validators.required]}],
+      password: [{value: 'welcome', disabled: true}],
+      nombre: ['', {validators: [Validators.required]}],
+      apellido: ['', {validators: [Validators.required]}],
+      cedula: [null],
+      correo: ['', {validators: [Validators.required]}],
+      telefono: [''],
+      celular: [''],
+      rollId: ['', {validators: [Validators.required]}],
+      direccion: [''],
+      cargo: [''],
+      departamento: [''],
+      createdAt: this.dateService.getDate(),
+      check_activo: [false],
+
+      proyecto: ['', {validators: [Validators.required]}],
+      version: [this.hoy, [Validators.required]],
+      solicitar_acceso: [false],
+      actualizar_acceso: [false],
+      eliminar_acceso: [false],
+      manos_remotas: [false],
+      alertas_baja: [false],
+      alertas_media: [false],
+      alertas_alta: [false],
+      editar_matriz: [false],
+      proyectos: this.formBuilder.array([]), // FormArray
+      link: ['', {validators: [Validators.required]}]
+    });
+
     this.getUserRoll();
+    if (this.userroll === 1) {
+      this.getRoles();
+    }
   }
 
   get proyectos(){
     return this.mdcForm.get('proyectos') as FormArray;
   }
 
+  // Obtiene del localstorage el roll del usuario logeado
   getUserRoll() {
     this.userroll = this.localstorageService.getUserRoll();
-    console.log(this.userroll);
+    if (this.userroll === 2) {
+      this.mdcForm.patchValue({
+        rollId: 2,
+      });
+    }
   }
+
+  getRoles(): void {
+    this.generalService.select(`ppppccccc`, `sp_select_roles()`).subscribe((resp: any) => {
+      this.roles = resp.select;
+    });
+  }
+
+  onSubmit(){
+    if (!this.mdcForm.valid){
+      alert('Fomulario no válido');
+      return;
+    }
+    // this.agregarUsuarioMatriz(this.token,this.mdcForm.getRawValue());
+    console.log(this.mdcForm.getRawValue());
+  }
+
 
   onChange(event: any): void {
   }
@@ -65,7 +95,7 @@ export class MdcformComponent implements OnInit {
     const mdcFormGroup = this.formBuilder.group({
       proyecto: ['', {validators: [Validators.required]}],
       link: ['', {validators: [Validators.required]}],
-      version: [this.dateService.actualYear(), {validators: [Validators.required, Validators.min(2000),
+      version: [this.hoy, {validators: [Validators.required, Validators.min(2000),
         Validators.max(this.dateService.actualYear() + 1)]}],
       solicitar_acceso: [false],
       actualizar_acceso: [false],
--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -29,6 +29,22 @@ export class DateService {
     return this.hoy;
   }
 
+  public lastDayYear() {
+    // return new Date(new Date().getFullYear(), 11, 31);
+    this.hoy = new Date();
+    this.yyyy = this.hoy.getFullYear();
+    const lastDay = `${this.yyyy}-12-31`;
+    return lastDay;
+  }
+
+  public firstDayNextYear() {
+    const hoy: any = new Date();
+    const yyyy: any = hoy.getFullYear();
+    const nextYear = `${yyyy + 1}-01-01`;
+    return nextYear;
+    // console.log(`${yyyy+1}-01-01`)
+  }
+
   actualYear(): number {
     this.hoy = new Date();
     const actualYear = this.hoy.getFullYear();
