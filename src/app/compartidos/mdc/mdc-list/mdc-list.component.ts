c134b05a698fdfa77137ce286a561ec54c5d21fe
AbdielP <abdiel.pinzonc@gmail.com>
2021-03-09 19:02:39 -0500

    se lista MDC en tablas de componente mdc-list. Se corrigen estilos. penmd iente modificar tabla de roles

--- src/app/compartidos/mdc/mdc-list/mdc-list.component.ts
+++ src/app/compartidos/mdc/mdc-list/mdc-list.component.ts
@@ -1,5 +1,6 @@
 import { Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-mdc-list',
@@ -13,18 +14,33 @@ export class MdcListComponent implements OnInit {
   versionesMatriz = '';
   eventSubject: Subject<any> = new Subject<any>();
 
-  constructor() { }
+  constructor(private generalService: GeneralService) { }
 
   ngOnInit(): void {
   }
 
    // Recibe el proyecto emitido desde el componmente hijo: app-proyectos cuando se usa el <select>
    getProyecto(proyecto): void {
+    this.nombreEmpresa = JSON.parse(proyecto).nombre_empresa;
     this.getVersion(JSON.parse(proyecto).idproyecto);
+    this.getListadoMDC(JSON.parse(proyecto).idproyecto);
   }
 
+  // Obtiene las versiones de MDC del proyecto del usuario
   getVersion(idproyecto: any) {
     console.log(idproyecto);
+    this.generalService.select('ppppccccc', `select_versiones_matrices(${idproyecto})`).subscribe((resp: any) => {
+      // console.log(resp.select);
+      this.versionesMatriz = resp.select;
+    });
+  }
+
+  // Obtiene el listado de personal de la MDC más reciente del usuario logeado
+  getListadoMDC(idproyecto: number) {
+    this.generalService.select('ppppccccc', `sp_select_mdc_proyecto(${idproyecto})`).subscribe((resp: any) => {
+      // console.log(resp);
+      this.usuarios = resp.select;
+    });
   }
 
   redirect(link: string) {}
