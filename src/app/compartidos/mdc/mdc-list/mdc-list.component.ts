0fd45956558ac5c38e55bc1131091bc919212f06
AbdielP <abdiel.pinzonc@gmail.com>
2021-03-09 23:47:10 -0500

    Se listan usuarios de MDC según proyecto y versiones. se modifica tabla de roles para que sea responsive

--- src/app/compartidos/mdc/mdc-list/mdc-list.component.ts
+++ src/app/compartidos/mdc/mdc-list/mdc-list.component.ts
@@ -1,37 +1,53 @@
+import { DatePipe } from '@angular/common';
 import { Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { GeneralService } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-mdc-list',
+  providers: [DatePipe],
   templateUrl: './mdc-list.component.html',
   styleUrls: ['./mdc-list.component.css']
 })
 export class MdcListComponent implements OnInit {
 
+  proyecto: any = [];
   nombreEmpresa = '';
   usuarios: any;
   versionesMatriz = '';
   eventSubject: Subject<any> = new Subject<any>();
 
-  constructor(private generalService: GeneralService) { }
+  constructor(private generalService: GeneralService, public datePipe: DatePipe) { }
 
   ngOnInit(): void {
   }
 
-   // Recibe el proyecto emitido desde el componmente hijo: app-proyectos cuando se usa el <select>
-   getProyecto(proyecto): void {
-    this.nombreEmpresa = JSON.parse(proyecto).nombre_empresa;
-    this.getVersion(JSON.parse(proyecto).idproyecto);
-    this.getListadoMDC(JSON.parse(proyecto).idproyecto);
+  // Recibe el proyecto emitido desde el componmente hijo: app-proyectos cuando se usa el <select>
+  getProyecto(proyecto): void {
+    this.proyecto = JSON.parse(proyecto);
+    this.nombreEmpresa = this.proyecto.nombre_empresa;
+    this.getVersiones(this.proyecto.idproyecto);
+    this.getListadoMDC(this.proyecto.idproyecto);
   }
 
   // Obtiene las versiones de MDC del proyecto del usuario
-  getVersion(idproyecto: any) {
+  getVersiones(idproyecto: any) {
     console.log(idproyecto);
     this.generalService.select('ppppccccc', `select_versiones_matrices(${idproyecto})`).subscribe((resp: any) => {
       // console.log(resp.select);
       this.versionesMatriz = resp.select;
+    });
+  }
+
+  // Obtiene LA versión seleccionada de MDC
+  getVersion(event: any) {
+    // console.log(this.datePipe.transform(event.target.value, 'yyyy-MM-dd'));
+    console.log(this.proyecto.idproyecto)
+    const fecha = this.datePipe.transform(event.target.value, 'yyyy-MM-dd');
+    this.generalService.select('ppppccccc', `sp_select_mdc_proyecto_version(${this.proyecto.idproyecto},'${fecha}')`)
+    .subscribe((resp: any) => {
+      console.log(resp);
+      this.usuarios = resp.select;
     });
   }
 
@@ -43,6 +59,8 @@ export class MdcListComponent implements OnInit {
     });
   }
 
-  redirect(link: string) {}
+  redirect(link: string) {
+    window.open(link, '_blank');
+  }
 
 }
