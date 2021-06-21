cefc1212be8ed12af1231fa38e6dac17bd42d884
AbdielP <abdiel.pinzonc@gmail.com>
2021-06-21 02:57:15 -0500

    Se agrega spinner y contador para redirección al componente firstpassword. pendiente lo mismo para preguntas de seguridad

--- src/app/auth/changepass/changepass.component.ts
+++ src/app/auth/changepass/changepass.component.ts
@@ -1,11 +1,13 @@
 import { Router } from '@angular/router';
 import { LocalstorageService } from 'src/app/services/localstorage/localstorage.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { CustomValidators } from 'src/app/utils/custom-validators';
 import { GeneralService } from 'src/app/services/general.service';
-import { Subject } from 'rxjs';
 import { SeguridadService } from 'src/app/services/seguridad.service';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-changepass',
@@ -20,8 +22,11 @@ export class ChangepassComponent implements OnInit{
   hiddeQuestions = false;
   form: FormGroup;
 
+  showSpinner = false;
+  counter = 5;
+
   constructor(private router: Router, private localstorageservice: LocalstorageService , private generalService: GeneralService,
-              private seguridadService: SeguridadService) {
+              private seguridadService: SeguridadService, private authService: AuthService) {
     this.form = new FormGroup({
       password: new FormControl('', [Validators.required, CustomValidators.longitud, CustomValidators.number,
         CustomValidators.lowerCase, CustomValidators.uppercase, CustomValidators.specialCharacter]),
@@ -38,6 +43,7 @@ export class ChangepassComponent implements OnInit{
   }
 
   onSubmit() {
+    this.showSpinner = true;
     const formPassword = this.form.value;
     if (this.questionsCount < 3) {
       // emitir el formulario hacia el componente 'preguntas' y mostrar el form preguntas
@@ -52,7 +58,17 @@ export class ChangepassComponent implements OnInit{
     // NO PUEDO ACTUALIZAR EL TOKEN, Los guard no lo detectan...
     this.generalService.update(url, form, this.localstorageservice.getToken()).subscribe((resp: any) => {
       if (resp.ok) {
-        console.log(resp.message);
+        this.counter = 5;
+        const interval = setInterval(() => {
+          this.swalAlert(resp.message);
+          this.counter--;
+          if (this.counter === 0) {
+            clearInterval(interval);
+            // REDIRECCIONA AQUÍ! ...
+            this.authService.logOut();
+          }
+          console.log(this.counter);
+        }, 1000);
       }
       // if (resp.usuario.roll === 'cliente') {
       //   this.router.navigate([`/`]);
@@ -69,6 +85,16 @@ export class ChangepassComponent implements OnInit{
     this.seguridadService.select(`api/cwpidc/usersq?token=${this.localstorageservice.getToken()}`)
     .subscribe((resp: any) => {
       this.questionsCount = resp.select[0].registros;
+    });
+  }
+
+  private swalAlert(message: string): void {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: message,
+      showConfirmButton: false,
+      timer: 1500
     });
   }
 }
