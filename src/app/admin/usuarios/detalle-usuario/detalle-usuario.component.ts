a49c353e7df3d1c0e639ed34cb5570b4422fd07b
AbdielP <abdiel.pinzonc@gmail.com>
2021-07-08 23:21:45 -0500

    Se colocan datos del usuario en el componente de detalles. se implementa form para cambiar password

--- src/app/admin/usuarios/detalle-usuario/detalle-usuario.component.ts
+++ src/app/admin/usuarios/detalle-usuario/detalle-usuario.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
 import { Component, Input, OnInit } from '@angular/core';
 import { GeneralService } from 'src/app/services/general.service';
@@ -13,9 +13,15 @@ export class DetalleUsuarioComponent implements OnInit {
 
   @Input() events: Observable<any>;
   eventSubscription: Subscription;
+  usuario: any = '';
   form: FormGroup;
 
-  constructor(private generalService: GeneralService, private localStorageService: LocalstorageService) { }
+  constructor(private generalService: GeneralService, private localStorageService: LocalstorageService) {
+    this.form = new FormGroup({
+      password: new FormControl(''),
+      re_password: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
     this.subscribeEventIdUsuario();
@@ -31,10 +37,18 @@ export class DetalleUsuarioComponent implements OnInit {
       const usuario = {idusuario};
       this.generalService.post(`api/cwpidc/portal/userinfo?token=${this.localStorageService.getToken()}`, usuario).subscribe(resp => {
         console.log(resp.select);
+        this.usuario = resp.select;
       });
     });
   }
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+
+  activar(): void{}
+  desactivar(): void{}
+  bloquear(): void{}
+  desbloquear(): void{}
 
 }
